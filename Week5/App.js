f52a0f1382491cece847ed73a83315329abc17f4
luckyleonard <dzhu31@hotmail.com>
2020-01-23 19:48:50 +1100

    init express

---
+++ Week5/App.js
@@ -0,0 +1,16 @@
+// const name = 'mason';
+// const age = 104;
+
+// console.log(`my name is ${name}. my age is ${age}!`)
+
+const fs = require('fs');
+const http = requrie('http');
+
+http.createServer((request,respone) => {
+
+  if(request.url.indexOf('mecca')>0){
+    
+
+  }
+
+})
