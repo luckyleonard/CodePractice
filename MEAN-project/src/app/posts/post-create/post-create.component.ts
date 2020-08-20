4c89780d3d31e544fa40d8a87a82144d4d70219a
luckyleonard <dzhu31@hotmail.com>
2020-08-21 00:22:21 +1000

    📦 (add) try one way binding

--- MEAN-project/src/app/posts/post-create/post-create.component.ts
+++ MEAN-project/src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { LifecycleHooks } from '@angular/compiler/src/lifecycle_reflector';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
 })
-export class PostCreateComponent {}
+export class PostCreateComponent {
+  postContent = '';
+  onAddPost(postInput: HTMLTextAreaElement) {
+    console.dir(postInput);
+    this.postContent = postInput.value;
+  }
+}
