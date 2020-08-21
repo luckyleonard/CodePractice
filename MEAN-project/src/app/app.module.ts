167fb56d1bc8ec11ea2e107a7eeb8a3c236761c5
luckyleonard <dzhu31@hotmail.com>
2020-08-21 15:35:34 +1000

    ✨ (feat) add material components to nice display

--- MEAN-project/src/app/app.module.ts
+++ MEAN-project/src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +18,10 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
