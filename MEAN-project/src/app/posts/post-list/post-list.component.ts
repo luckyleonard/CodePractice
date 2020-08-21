b8e03a81395907a83f37334c75b8a316cd41136d
luckyleonard <dzhu31@hotmail.com>
2020-08-22 00:23:51 +1000

    ✨ (feat) dynamic post list with build-in diretives

--- MEAN-project/src/app/posts/post-list/post-list.component.ts
+++ MEAN-project/src/app/posts/post-list/post-list.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  // posts = [
+  //   {
+  //     title: 'first post',
+  //     content: 'this is the content for 1st',
+  //   },
+  // ];
+  posts = [];
+  ngOnInit(): void {}
 }
